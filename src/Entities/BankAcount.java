f48921921f53f28a1a9a57b7986f729b16f19a85
Dan-dev1305 <Administrator@ROMERITO.AD>
2025-09-18 13:10:31 -0300

    Adição de mais lógica de negócio da retirada

--- src/Entities/BankAcount.java
+++ src/Entities/BankAcount.java
@@ -20,6 +20,10 @@ public class BankAcount {
         return saldo;
     }
 
+    public void setTitularName(String titularName) {
+        this.titularName = titularName;
+    }
+
     public BankAcount(int idAcount, String titularName, double initialDeposit) {
         this.idAcount = idAcount;
         this.titularName = titularName;
@@ -32,9 +36,11 @@ public class BankAcount {
 
     }
     public double depositValue(double deposit){
+        saldo = initialDeposit;
         return saldo += deposit;
     }
     public double withdraw(double withdraw){
+        return saldo -= withdraw + 5.00;
 
     }
 }
